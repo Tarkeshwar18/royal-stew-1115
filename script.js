86a591e870816e8a51c9ab6547856df25b51555c
Narender <narender24681@gmail.com>
2023-01-22 23:57:06 +0530

    nav

--- script.js
+++ script.js
@@ -11,46 +11,59 @@ percent.addEventListener("click",()=>{
     window.location.assign('./product.html')
 })
 
-
 // nav code starts here
 let newArrivalsLink = document.getElementById("new-arrivals-link");
-newArrivalsLink.addEventListener("click", () => {
+newArrivalsLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  
   console.log("newArrivalsLink");
   window.location.href = "./new-arrivals.html";
 });
 
 let casesSleevesLink = document.getElementById("cases-sleeves-link");
-casesSleevesLink.addEventListener("click", () => {
+casesSleevesLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  
   console.log("casesSleevesLink");
   window.location.href = "./cases-sleeves.html";
 });
 
 let accessoriesLink = document.getElementById("accessories-link");
-accessoriesLink.addEventListener("click", () => {
+accessoriesLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  
   console.log("accessoriesLink");
   window.location.href = "./accessories.html";
 });
 
 let saleLink = document.getElementById("sale-link");
-saleLink.addEventListener("click", () => {
+saleLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  
   console.log("saleLink");
   window.location.href = "./sale.html";
 });
 
 let bagsWalletsLink = document.getElementById("bags-wallets-link");
-bagsWalletsLink.addEventListener("click", () => {
+bagsWalletsLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  
   console.log("bagsWalletsLink");
   window.location.href = "./bags-wallets.html";
 });
 
 let homeOfficeLink = document.getElementById("home-office-link");
-homeOfficeLink.addEventListener("click", () => {
+homeOfficeLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  
   console.log("homeOfficeLink");
   window.location.href = "./home-office.html";
 });
 
 let collectionsLink = document.getElementById("collections-link");
-collectionsLink.addEventListener("click", () => {
+collectionsLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  
   console.log("collectionsLink");
   window.location.href = "./collections.html";
 });
