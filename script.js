fae49b8efc0830e1b447e648175203c75a435bbb
Narender <narender24681@gmail.com>
2023-01-22 10:18:38 +0530

    added links

--- script.js
+++ script.js
@@ -10,3 +10,48 @@ links.forEach(btn => btn.addEventListener("click",(e)=>{
 percent.addEventListener("click",()=>{
     window.location.assign('./product.html')
 })
+
+
+// nav code starts here
+let newArrivalsLink = document.getElementById("new-arrivals-link");
+newArrivalsLink.addEventListener("click", () => {
+  console.log("newArrivalsLink");
+  window.location.href = "./new-arrivals.html";
+});
+
+let casesSleevesLink = document.getElementById("cases-sleeves-link");
+casesSleevesLink.addEventListener("click", () => {
+  console.log("casesSleevesLink");
+  window.location.href = "./cases-sleeves.html";
+});
+
+let accessoriesLink = document.getElementById("accessories-link");
+accessoriesLink.addEventListener("click", () => {
+  console.log("accessoriesLink");
+  window.location.href = "./accessories.html";
+});
+
+let saleLink = document.getElementById("sale-link");
+saleLink.addEventListener("click", () => {
+  console.log("saleLink");
+  window.location.href = "./sale.html";
+});
+
+let bagsWalletsLink = document.getElementById("bags-wallets-link");
+bagsWalletsLink.addEventListener("click", () => {
+  console.log("bagsWalletsLink");
+  window.location.href = "./bags-wallets.html";
+});
+
+let homeOfficeLink = document.getElementById("home-office-link");
+homeOfficeLink.addEventListener("click", () => {
+  console.log("homeOfficeLink");
+  window.location.href = "./home-office.html";
+});
+
+let collectionsLink = document.getElementById("collections-link");
+collectionsLink.addEventListener("click", () => {
+  console.log("collectionsLink");
+  window.location.href = "./collections.html";
+});
+// nav code ends here
