d202bdfb325b90432320fed7a79b68ebdeb96533
Shahid Raza <112757232+Shahid0143@users.noreply.github.com>
2023-01-22 07:01:22 -1200

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,5 +1,7 @@
-function success() {
-    alert("Payment Succesfull!")
-    window.location.assign("./index.html");
 
-  }
+let form=document.querySelector('form')
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  alert('Payment Succesfull!')
+  window.location.assign("./index.html");
+})
